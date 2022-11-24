ebf49d5f8f140c447141bf90f143d35298211bbc
Jeong Yeon Jae <jyj010818@naver.com>
2022-11-25 00:32:54 +0900

    Study React

---
+++ React/my-app/src/chapter_10/AttendanceBook.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const students = [
+    {
+        id:1,
+        name: "Inje",
+    },
+    {
+        id:2,
+        name:"Steve",
+    },
+    {
+        id:3,
+        name:"John",
+    },
+    {
+        id:4,
+        name:"Jeff",
+    },
+
+];
+
+function AttendanceBook(props){
+    return (
+        <ul>
+            {students.map((student) => {
+                return <li key={student.id}>{student.name}</li>;
+            })}
+        </ul>
+    );
+}
+
+export default AttendanceBook;
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -9,10 +9,12 @@ import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import  ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <SignUp />
   </React.StrictMode>,
   document.getElementById('root')
 );
