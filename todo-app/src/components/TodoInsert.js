444a6af7558efbb4e330bd9f066fb7b05e284225
Jeong Yeon Jae <jyj010818@naver.com>
2022-11-30 01:42:02 +0900

    Study React

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,31 @@
+import { useState, useCallback } from 'react';
 import {MdAdd} from 'react-icons/md';
+//아이콘 추가
 import './TodoInsert.scss';
+//scss 확장자 => 스타일 시트 불러오기
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+    const[value, setValue] = useState('');
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e=>{
+            onInsert(value);
+            setValue('');
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
     return (
-        <form className="TodoInsert">
-            <input placeholder='할 일을 입력하세요' />
-            <button type='submit'>
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+            placeholder='할 일을 입력하세요' 
+            value={value}
+            onChange={onChange}
+            />
+            <button type="submit">
                 <MdAdd/>
             </button>
         </form>
