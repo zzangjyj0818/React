670ce1dc7207bb8de3cf68b8b32a810073391649
Jeong Yeon Jae <jyj010818@naver.com>
2022-12-31 00:26:17 +0900

    Clone Coding

---
+++ twitter_clone/src/components/Router.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    return(
+        <Router>
+            <Routes>
+                {isLoggedIn ? (
+                    <Route exact path = "/" element={<Home/>}>
+                    </Route>
+                ) : (
+                    <Route exact path = "/" element={<Auth/>}>
+                    </Route>
+                )}
+            </Routes>
+        </Router>
+    );
+}
+
+export default AppRouter;
