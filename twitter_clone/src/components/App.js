cba6f6dac9f75b0b01a1e00a23ee2c64f39dafa2
Jeong Yeon Jae <jyj010818@naver.com>
2023-01-03 22:38:41 +0900

    Clone

--- twitter_clone/src/components/App.js
+++ twitter_clone/src/components/App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import AppRouter from "components/Router";
+import {authService} from "fbase";
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUsers);
+  console.log(authService.currentUsers);
   return(
     <>
       <AppRouter isLoggedIn={isLoggedIn}/>
--- twitter_clone/src/components/Router.js
+++ twitter_clone/src/components/Router.js
@@ -1,19 +1,15 @@
-import { useState } from "react";
-import {HashRouter as Router, Route, Routes} from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
 
-const AppRouter = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({isLoggedIn}) => {
     return(
         <Router>
             <Routes>
                 {isLoggedIn ? (
-                    <Route exact path = "/" element={<Home/>}>
-                    </Route>
+                    <Route exact path = "/" element={<Home/>}/>
                 ) : (
-                    <Route exact path = "/" element={<Auth/>}>
-                    </Route>
+                    <Route exact path = "/" element={<Auth/>}/>
                 )}
             </Routes>
         </Router>
